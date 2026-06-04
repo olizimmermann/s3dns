b8dc688020d3da32f1262542e77a9be663088089
olizimmermann <github@ozimmermann.com>
2026-06-04 14:08:48 +0200

    updated version number

--- s3dns.py
+++ s3dns.py
@@ -32,7 +32,7 @@ import yaml
 
 # configuration possible below
 
-version = "0.2.14"
+version = "0.2.15"
 logo = r"""
    _____ ____    _____  _   _  _____   _____       _            _
   / ____|___ \  |  __ \| \ | |/ ____| |  __ \     | |          | |
